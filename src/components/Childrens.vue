6dd146c859850c957a327eba4c4312c99865e507
Dmitriy <dabrun@gmail.com>
2022-02-15 23:37:25 +0300

    add animations

--- src/components/Childrens.vue
+++ src/components/Childrens.vue
@@ -2,34 +2,40 @@
   <div>
     <div class="children__wrapper">
       <h3 class="children__title">Дети (макс. 5)</h3>
-      <button
-        v-if="childrens.length < 5"
-        class="children__add"
-        @click="addChildren"
-      >
-        <svg
-          width="18"
-          height="18"
-          viewBox="0 0 18 18"
-          xmlns="http://www.w3.org/2000/svg"
+      <transition name="fade">
+        <button
+          v-if="childrens.length < 5"
+          class="children__add"
+          @click="addChildren"
         >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M2.13332 7.85555C1.50125 7.85555 0.988866 8.36794 0.988867 9C0.988867 9.63206 1.50126 10.1445 2.13332 10.1445L7.85544 10.1445L7.85545 15.8668C7.85545 16.4989 8.36784 17.0113 8.9999 17.0113C9.63196 17.0113 10.1444 16.4989 10.1444 15.8668L10.1443 10.1445L15.8667 10.1445C16.4988 10.1445 17.0112 9.63207 17.0112 9.00001C17.0112 8.36795 16.4988 7.85556 15.8667 7.85556L10.1443 7.85556L10.1443 2.13338C10.1443 1.50132 9.63195 0.988927 8.99989 0.988927C8.36783 0.988927 7.85544 1.50131 7.85544 2.13338L7.85544 7.85555L2.13332 7.85555Z"
-          />
-        </svg>
-        Добавить ребенка
-      </button>
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M2.13332 7.85555C1.50125 7.85555 0.988866 8.36794 0.988867 9C0.988867 9.63206 1.50126 10.1445 2.13332 10.1445L7.85544 10.1445L7.85545 15.8668C7.85545 16.4989 8.36784 17.0113 8.9999 17.0113C9.63196 17.0113 10.1444 16.4989 10.1444 15.8668L10.1443 10.1445L15.8667 10.1445C16.4988 10.1445 17.0112 9.63207 17.0112 9.00001C17.0112 8.36795 16.4988 7.85556 15.8667 7.85556L10.1443 7.85556L10.1443 2.13338C10.1443 1.50132 9.63195 0.988927 8.99989 0.988927C8.36783 0.988927 7.85544 1.50131 7.85544 2.13338L7.85544 7.85555L2.13332 7.85555Z"
+            />
+          </svg>
+          Добавить ребенка
+        </button>
+      </transition>
     </div>
-    <children-form
-      v-for="item in childrens"
-      :key="item.id"
-      v-bind:item="item"
-    />
-    <button class="children__save" v-if="childrens.length !== 0">
-      Сохранить
-    </button>
+    <transition-group name="list" tag="p">
+      <children-form
+        v-for="item in childrens"
+        :key="item.id"
+        v-bind:item="item"
+      />
+    </transition-group>
+    <transition name="fade">
+      <button class="children__save" v-if="childrens.length !== 0">
+        Сохранить
+      </button>
+    </transition>
   </div>
 </template>
 
@@ -103,5 +109,20 @@ export default {
   color: #01a7fd;
   background-color: #fff;
   border: 1px solid #01a7fd;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
 }
 </style>
